6a52f769b5cf6db6bb151f04a14e28269bd097ec
Kenny Kerr <kenny@kennykerr.ca>
2022-05-02 14:16:48 -0700

    sample

---
+++ cswinrt/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var c = new Component.Class();
+        c.Property = 123;
+        Debug.Assert(c.Property() == 123);
+    }
+}

5dd8064f72ee4aed30f946be8bda95c7d4748c93
Manodasan Wignarajah <mawign@microsoft.com>
2022-05-02 15:30:16 -0700

    Add NetFramework sample

---
+++ netframework/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netframework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Component.Class();
+            c.Property = 123;
+            System.Diagnostics.Debug.Assert(c.Property == 123);
+            System.Console.WriteLine("ok!");
+        }
+    }
+}
